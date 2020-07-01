798026a21c46e59396842e09be030ece07549077
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-07-01 13:35:24 +0100

    added App.vue components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  
+  <div>
+    <bookings-list></bookings-list>
+    <booking-form></booking-form>
+  </div>
 </template>
 
 <script>
+
+import BookingsList from "./components/BookingsList.vue"
+import BookingForm from "./components/BookingForm.vue"
+
 export default {
+  name: 'app',
+  components: {
+    "bookings-list": BookingsList,
+    "booking-form": BookingForm,
+  }
 
 }
 </script>
