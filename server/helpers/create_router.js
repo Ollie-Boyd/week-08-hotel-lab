4bd54fbe28a38dab773dea50ce2f90d946bdc1a0
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-07-01 11:44:25 +0100

    added GET to create_router.js

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -4,9 +4,23 @@ const ObjectID = require('mongodb').ObjectID;
 const createRouter = function (collection) {
 
   const router = express.Router();
+    
+    router.get('/', (req, res) => {
+        collection.find()
+        .toArray()
+        .then((returned) => res.json(returned))
+        .catch((err) => {
+            console.log(err)
+            res.status(500)
+            res.json({status: 500, error: err})
+          })
 
+    
+    } )
 
   return router;
+
+
 
 };
 
