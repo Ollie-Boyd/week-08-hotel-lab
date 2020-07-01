0020f5b067e24cdb19605180aef8469880a9de58
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-07-01 13:27:12 +0100

    File structure for client restructured

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -28,6 +28,18 @@ const createRouter = function (collection) {
         })
     })
 
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id
+        collection.deleteOne({_id: ObjectID(id)})
+        .then((returned) => res.json(returned))
+        .catch((err) => {
+            console.log(err)
+            res.status(500)
+            res.json({status: 500, error: err})
+        })
+
+    })
+
   return router;
 
 
