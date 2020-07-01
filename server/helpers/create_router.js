7d98debdcbc295009cfe1489b8dfc6068538f4df
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-07-01 11:55:57 +0100

    created show route in create_router

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -14,9 +14,19 @@ const createRouter = function (collection) {
             res.status(500)
             res.json({status: 500, error: err})
           })
+    })
 
-    
-    } )
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+        .findOne({_id: ObjectID(id)})
+        .then((returned) => res.json(returned))
+        .catch((err) => {
+            console.log(err)
+            res.status(500)
+            res.json({status: 500, error: err})
+        })
+    })
 
   return router;
 
